7d4ca4a45012e663ab3a5acc71d8c9c9ba3f5d9c
todrepus <superdot@naver.com>
2022-06-03 17:10:54 +0900

    apply bootstrap design

---
+++ src/기존html/js/map.js
@@ -0,0 +1,151 @@
+import { REST_API_KEY } from './settings.js';
+import {Session, agContract} from './session.js';
+import {Reserv} from './reserv.js';
+
+
+const MapHelper = {
+    juso_arr : [],
+    points : ['', ''], // 출발지, 목적지
+    now_target_pos : 0,
+
+    init: function(){
+        const juso_points = sessionStorage.getItem('juso_points');
+        console.log(juso_points);
+        if (juso_points){
+            const point_map = JSON.parse(juso_points);
+            this.points[0] = point_map.sp;
+            this.points[1] = point_map.dp;
+        }
+
+        this.updateInputs();
+    },
+    search: async function(juso) {
+        let url = 'https://dapi.kakao.com/v2/local/search/address.json';
+        const analyze_type = 'similar';
+        const size = 30;
+
+        url = url + `?analyze_type=${analyze_type}&size=${size}&query=${juso}`;
+        url = encodeURI(url);
+        fetch(url, {
+            method: "GET",
+            headers: {
+                "Authorization" : `KakaoAK ${REST_API_KEY}`
+            }
+        })
+        .then((response) => response.json())
+        .then((data) => this.updateModal(data));
+    },
+
+    //2. 지도 표기 함수 (주어진 위치를 지도에 표기)
+    updateModal: function (data){
+        this.juso_arr = data.documents;
+        console.log(this.juso_arr);
+        
+        const ul = document.getElementById('juso_ul');
+        ul.innerHTML = '';
+
+        let i = 0;
+        this.juso_arr.forEach(
+            juso => {
+                const li_str = 
+                `
+                <button class="list-group-item list-group-item-action" onclick="MapHelper.updateDetail(${i})">${juso.road_address.address_name}</button>
+                `
+                const li = document.createElement('li');
+                li.innerHTML = li_str;
+                ul.appendChild(li);
+                i+=1;
+            }
+        )
+    },
+
+    updateDetail: function(i){
+        const juso = this.juso_arr[i];
+        document.getElementById('detail').style.visibility = "visible";
+        document.getElementById('selected_juso').innerHTML = juso.road_address.address_name;
+        document.getElementById('addBtn').onclick = () => {
+            this.points[this.now_target_pos] = `${juso.road_address.address_name} ${document.getElementById('juso_detail').value}`;
+            this.updateInputs();
+        }
+    },
+
+    updateInputs: function(){
+        const sp = document.getElementById('start_point');
+        const dp = document.getElementById('dest_point');
+
+        sp.value = this.points[0];
+        dp.value = this.points[1];
+    },
+
+    completeReserv: async function (){
+        if (this.points[0] === ''){
+            alert('출발지를 입력해주세요.');
+            return;
+        }
+        if (this.points[1] === ''){
+            alert('목적지를 입력해주세요.');
+            return;
+        }
+
+        const juso_points = {
+            sp: this.points[0],
+            dp: this.points[1]
+        };
+        
+        sessionStorage.setItem('juso_points', JSON.stringify(juso_points));
+        
+        const success = await Reserv.push(this.points[0], this.points[1]);
+        if (success)
+            location.href = '예약완료.html';
+    }
+}
+
+window.MapHelper = MapHelper;
+
+window.onload = () =>{
+    const jusoModal = document.getElementById('jusoModal');
+    
+    jusoModal.addEventListener('show.bs.modal', function (event){
+        const input = event.relatedTarget;
+        const from = input.getAttribute('data-bs-from');
+
+        let i = 0; // 
+        if (from === "dest_point"){
+            i = 1; // 목적지로 설정
+        }
+        MapHelper.now_target_pos = i;
+    });
+
+    jusoModal.addEventListener('hidden.bs.modal', function (event){
+        document.getElementById('juso_input').value = '';
+        document.getElementById('selected_juso').value = '';
+        document.getElementById('juso_detail').value = '';
+
+        document.getElementById('detail').style.visibility = 'hidden';
+        document.getElementById('juso_ul').innerHTML = '';
+
+
+    });
+
+    MapHelper.init();
+
+    (async() => {
+        let can_reserv = true;
+        try{
+            const result = await agContract.methods.getLastReservation(Session.auth.wallet_address).call();
+            console.log(result);
+            if (result.cancelled || result.pay_completed){
+            }else{
+                can_reserv = false;
+            }
+        }catch (error){
+
+        }
+
+        if (!can_reserv){
+            location.href = "예약완료.html";
+        }
+        const btn = document.getElementById('reservBtn');
+        btn.disabled = false;
+    })();
+}
---
+++ src/기존html/js/reserv.js
@@ -0,0 +1,147 @@
+import {agContract, Session} from './session.js';
+import {MAX_ELAPSED_SEC} from './settings.js';
+
+// 1. 예약정보 저장할 공간
+export const Reserv = {
+    data: {
+        occur : 0,
+        start : 0,
+        end : 0,
+        paid : 0,
+        pay_completed : false,
+        cancelled : false,
+        start_point : '',
+        dest_point : ''
+    },
+    update: async function(){ // 예약정보 업데이트
+        try{
+            const result = await agContract.methods.getLastReservation(Session.auth.wallet_address).call();
+            console.log(result);
+            this.data.occur = result.occur;
+            this.data.start = result.start;
+            this.data.end = result.end;
+            this.data.paid = result.paid;
+            this.data.pay_completed = result.pay_completed;
+            this.data.cancelled = result.cancelled;
+            this.data.start_point = result.start_point;
+            this.data.dest_point = result.dest_point;
+        }catch (error){
+            return false;
+        }
+        console.log(this);
+        return true;
+
+    },
+    // 2. 예약하기 (출발지, 목적지)
+    push: async function(sp, dp){
+        try{
+            const result = await agContract.methods.addReserv(sp, dp).send({from : Session.auth.wallet_address, gas:'500000'});
+            console.log(result);
+            return true;
+        }catch (error){
+            console.log(error);
+            return false;
+        }
+    },
+
+    checkInTaxiEventCallback : async function(){
+        const success = await this.update();
+        if (!success){
+            alert('Error');
+            return;
+        }
+        this.process();
+    },
+    checkOutTaxiEventCallback : async function(){
+        const success = await this.update();
+        if (!success){
+            alert('Error');
+            return;
+        }
+
+        alert('도착!')
+        this.process();
+    },
+
+    addCallback : function(){
+        agContract.events.checkInTaxiEvent({filter:{
+            addr: Session.auth.wallet_address
+        }}).on('data', function(event){
+            console.log('택시 출발체크인')
+            console.log(event);
+            Reserv.checkInTaxiEventCallback();
+        })
+        agContract.events.checkOutTaxiEvent({filter:{
+            addr: Session.auth.wallet_address
+        }}).on('data', function(event){
+            console.log('택시 도착 체크아웃')
+            Reserv.checkOutTaxiEventCallback();
+        })
+        console.log('callback is added');
+    },
+
+    process : function(){
+        if (!success){
+            return;
+        }
+        if (this.data.pay_completed){ // 결제가 모두 완료된경우
+            alert('예약이 끝에 도달함');
+            
+        }
+        else if (this.data.end != 0){ // 도착완료
+            alert('도착완료')
+            location.href = 'payment_complete.html';
+        }else if (this.data.start != 0){ // 운행중 (체크인)
+            document.getElementById('content_reserv').innerHTML = "운행중......";
+        }else if (this.data.cancelled){ // 취소인경우
+            alert('취소')
+            location.href = 'payment_cancel.html'
+        }
+    },
+
+    cancel: async function() {
+        const success = await this.update();
+        if (!success){
+            alert('정보 불러오기 실패');
+            return;
+        }
+
+        if (this.data.start != 0){
+            alert('출발 이후엔 취소 불가능합니다.');
+            return;
+        }
+
+        const now = new Date().getTime(); // 현재timestamp 
+        const elapsedSec = (now  - (this.data.start*1000)) / (1000); // 몇초 지났는가
+        if (elapsedSec > MAX_ELAPSED_SEC){ // 일정시간이상 지나면, 취소를 못하도록한다. ( 택시기사 보호차원 )
+            alert(`${MAX_ELAPSED_SEC/60}분 이후에는 취소가 불가능합니다.`);
+            return;
+        }
+
+        try{
+            const transaction = await agContract.methods.cancelLastReserv().send({from : Session.auth.wallet_address, gas:'500000'});
+            console.log(transcation);
+            alert('취소하였습니다.')
+            await this.update();
+            this.process();
+        }catch (error){
+            alert('취소에 실패했습니다.')
+            
+        }
+    }
+}
+
+
+// 3. 기사님 요청수락 콜백 이벤트 ( 예약완료 )
+// 기사님이 요청수락 하면, 실행될 함수
+
+// 4. 도착시 콜백 이벤트 (도착완료)
+// 기사님이 도착보내면, 실행될함수
+
+
+// 5. 콜백 등록
+
+
+
+
+window.Reserv = Reserv;
